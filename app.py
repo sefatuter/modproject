0c75b33eae29e0bab0ddd83a3d4c78998d1728b6
sefatuter <sefa2233_18@hotmail.com>
2025-01-18 15:19:13 +0300

    modsecurity setup and configurations

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, Response
+import time
 
 app = Flask(__name__)
 
@@ -11,3 +12,18 @@ def index():
 @app.route('/modsettings')
 def panel():
     return render_template('modsettings.html')
+
+@app.route('/stream_logs')
+def stream_logs():
+    log_file_path = '/var/log/modsec_audit.log'
+
+    def generate():
+        with open(log_file_path, 'r') as log_file:
+            log_file.seek(0, 2)  # Move to the end of the file
+            while True:
+                line = log_file.readline()
+                if line:
+                    yield f"data: {line.strip()}\n\n"
+                time.sleep(0.2)  # Prevent high CPU usage
+
+    return Response(generate(), content_type='text/event-stream')
